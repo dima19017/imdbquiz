a0223719c1a05d536a9fc7aa5eebbcc8d33b9c5c
shurtupin.d <shurtupin.d@wb.ru>
2024-11-17 16:42:42 +0300

    Не работает игровая комната. Откатить если что

--- flask/app.py
+++ flask/app.py
@@ -4,6 +4,7 @@ import os
 import sqlite3
 import random
 import requests
+import logging
 from functools import wraps
 from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session, url_for, jsonify
@@ -27,6 +28,10 @@ TMDB_API_URL = 'https://api.themoviedb.org/3'
 PROXY = "http://eWSWGwq8:dWAf82nT@166.1.128.144:64044"
 
 rooms = {}
+
+# Глобальный словарь для хранения данных об играх по ID комнат
+game_data = {}
+
 
 def apology(message, code=400):
     """Render message as an apology to user with a custom meme image."""
@@ -176,6 +181,79 @@ def room(room_id):
     else:
         return apology("Room not found", code=404)
 
+@app.route('/api/rooms', methods=['GET'])
+def get_rooms():
+    return jsonify(rooms)
+
+@app.route('/random_movie', methods=['GET'])
+def random_movie():
+    movie = {
+        'id': 12345,
+        'release_date': '2023-01-01',
+        'vote_average': 8.5,
+        'overview': 'A great movie!',
+        'original_title': 'Test Movie'
+    }
+    logging.info(f"Movie selected: {movie}")
+    return movie
+    #page = random.randint(1, 20)
+#
+    ## Формируем URL с параметрами запроса
+    #url = f"{TMDB_API_URL}/movie/popular?api_key={TMDB_API_KEY}&language=en-US&page={page}"
+    #logging.info(f"Fetching random movie from TMDB. URL: {url}")
+    ## Отправляем GET-запрос через requests
+    #try:
+    #    response = requests.get(url, proxies={"http": PROXY, "https": PROXY} if PROXY else None)
+    #    response.raise_for_status()  # Проверка успешности запроса
+#
+    #    movies_data = response.json()  # Парсим JSON
+    #    movies = movies_data.get('results', [])
+#
+    #    if movies:
+    #        random_movie = random.choice(movies)  # Случайный фильм из полученного списка
+    #        logging.info(f"Successfully fetched random movie: {random_movie['original_title']}")
+    #        return random_movie  # Отправляем случайный фильм в формате JSON
+    #    else:
+    #        logging.error("No movies found in the response.")
+    #        return jsonify({'error': 'No movies found'}), 404
+#
+    #except requests.exceptions.RequestException as e:
+    #    logging.error(f"Error fetching data from TMDB: {str(e)}")
+    #    return jsonify({'error': f'Error fetching data from TMDB: {str(e)}'}), 500
+
+@app.route('/game/<room_id>', methods=['GET'])
+@login_required
+def game(room_id):
+    logging.info(f"Game starting for room {room_id}. Checking if players are connected.")
+
+    if room_id not in rooms or len(rooms[room_id]['players']) == 0:
+        logging.error(f"No players connected to the room {room_id}.")
+        return apology("No players in the room.", code=400)
+    
+    # Получаем случайный фильм для игры
+    movie = random_movie()  # Вызов функции получения фильма
+    if 'error' in movie:  # Проверка на ошибку, если она возникла
+        logging.error(f"Error fetching random movie for room {room_id}: {movie.json.get('error')}")
+        return apology("Could not fetch movie details.", code=500)
+    
+    # Извлекаем подсказки
+    hints = {
+        'release_date': movie['release_date'],
+        'vote_average': movie['vote_average'],
+        'overview': movie['overview'],
+    }
+    logging.info(f"Prepared hints: {hints} for room {room_id}")
+    # Сохраняем правильный ответ (оригинальное название фильма)
+    game_data[room_id] = {
+        'correct_answer': movie['original_title'],
+        'players_answers': {}
+    }
+
+    logging.info(f"Game started in room {room_id}. Movie: {movie['original_title']}")
+    # Просто передаем подсказки для игры, без использования emit внутри HTTP-обработчика
+    return render_template('game.html', room_id=room_id, movie_id=movie['id'])
+
+
 # Обработчик для подключений через WebSocket
 @socketio.on('join')
 def on_join(data):
@@ -193,6 +271,8 @@ def on_join(data):
         if user_id not in rooms[room_id]['players']:
             rooms[room_id]['players'].append(user_id)
 
+        logging.info(f"User {user_id} joined room {room_id}. Current players: {rooms[room_id]['players']}")
+
         # Оповещаем комнату о новом пользователе и отправляем актуальный список игроков
         emit('message', {
             'msg': f'{user_id} has joined the room.',
@@ -200,8 +280,7 @@ def on_join(data):
         }, room=room_id)
         print(f"User {user_id} has joined the room {room_id}. Current players: {rooms[room_id]['players']}")
     else:
-        print(f"User {user_id} failed to join the room {room_id}.")
-
+        logging.error(f"Failed to join room {room_id} for user {user_id}")
 
 @socketio.on('leave')
 def on_leave(data):
@@ -249,42 +328,69 @@ def on_ready(data):
 def on_start_game(data):
     room_id = data.get('room_id')
     user_id = session.get('user_id')
+    logging.info(f"Received 'start_game' request from user {user_id} in room {room_id}.")
 
     if room_id and user_id:
+        # Проверяем, что только создатель может начать игру
         if rooms[room_id]['creator'] == user_id:
-            # Только создатель может начать игру
-            emit('game_started', {'msg': 'Game started!'}, room=room_id)
-            print(f"Game started in room {room_id} by {user_id}")
+            logging.info(f"User {user_id} is the creator of room {room_id}. Starting game.")
+            # Получаем случайный фильм
+            movie = random_movie()
+            hints = {
+                'release_date': movie['release_date'],
+                'vote_average': movie['vote_average'],
+                'overview': movie['overview']
+            }
+            logging.info(f"Sending hints: {hints} to room {room_id}.")
+            # Отправляем подсказки всем игрокам через WebSocket
+            emit('game_started', {'msg': 'The game has started!'}, room=room_id)
+            emit('show_hints', hints, room=room_id)
+
+            # Сохраняем данные о фильме и правильный ответ
+            game_data[room_id] = {
+                'correct_answer': movie['original_title'],
+                'players_answers': {}
+            }
+            logging.info(f"Game data saved for room {room_id}: {game_data[room_id]}")
         else:
             print(f"User {user_id} tried to start the game without being the creator.")
 
-@app.route('/api/rooms', methods=['GET'])
-def get_rooms():
-    return jsonify(rooms)
 
-@app.route('/random_movie', methods=['GET'])
-def random_movie():
-    page = random.randint(1, 20)
+@socketio.on('submit_answer')
+def on_submit_answer(data):
+    room_id = data['room_id']
+    user_id = session.get('user_id')
+    answer = data['answer']
+    
+    correct_answer = game_data[room_id]['correct_answer']
+    is_correct = answer.lower() == correct_answer.lower()
+    
+    # Сохраняем ответ игрока
+    game_data[room_id]['players_answers'][user_id] = is_correct
+    
+    # Отправляем статус игрока (правильный или неправильный ответ)
+    emit('player_answered', {
+        'user_id': user_id,
+        'is_correct': is_correct
+    }, room=room_id)
 
-    # Формируем URL с параметрами запроса
-    url = f"{TMDB_API_URL}/movie/popular?api_key={TMDB_API_KEY}&language=en-US&page={page}"
+@socketio.on('end_game')
+def on_end_game(data):
+    room_id = data['room_id']
+    
+    correct_players = [user for user, correct in game_data[room_id]['players_answers'].items() if correct]
+    incorrect_players = [user for user, correct in game_data[room_id]['players_answers'].items() if not correct]
+    
+    # Отправляем результаты игры
+    emit('game_results', {
+        'correct_players': correct_players,
+        'incorrect_players': incorrect_players
+    }, room=room_id)
+    
+    # Возвращаем игроков в комнату
+    emit('game_end', {'message': "Returning to room"}, room=room_id)
 
-    # Отправляем GET-запрос через requests
-    try:
-        response = requests.get(url, proxies={"http": PROXY, "https": PROXY} if PROXY else None)
-        response.raise_for_status()  # Проверка успешности запроса
 
-        movies_data = response.json()  # Парсим JSON
-        movies = movies_data.get('results', [])
-
-        if movies:
-            random_movie = random.choice(movies)  # Случайный фильм из полученного списка
-            return jsonify(random_movie)  # Отправляем случайный фильм в формате JSON
-        else:
-            return jsonify({'error': 'No movies found'}), 404
-
-    except requests.exceptions.RequestException as e:
-        return jsonify({'error': f'Error fetching data from TMDB: {str(e)}'}), 500
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=5000, debug=True)
